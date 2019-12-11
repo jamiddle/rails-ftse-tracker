7e1373d0bd987a4e74c8cc9dd0e5b61a3ce3619f
James Middlemass <james.middlemass01@gmail.com>
2019-12-11 14:09:23 +0000

    changed database from sqlite3 to postgres and updated to v.12

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_11_130214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stocks", force: :cascade do |t|
     t.string "symbol"
     t.string "name"
